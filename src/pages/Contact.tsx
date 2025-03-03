12abc54e2b82103d344f1997a89d299860d542db
Mehboob Ali <mehboobaliali150@gmail.com>
2025-03-03 21:23:37 +0500

    Updated code with modifications

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -407,7 +407,7 @@ const Contact = () => {
                 <iframe
                   title="TravelExplorer Office Location"
                   className="w-full h-full border-0"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387193.30596552044!2d-74.25986548248684!3d40.69714941932609!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sca!4v1619712000000!5m2!1sen!2sca"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.9813196006216!2d51.52923547447452!3d25.274755277661065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e45c543e288e80f%3A0x97c030534ba339c0!2sB%20Ring%20Rd%2C%20Doha%2C%20Qatar!5e1!3m2!1sen!2s!4v1741018900737!5m2!1sen!2s"
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
                   allowFullScreen
